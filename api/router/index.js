28f8d8aa6eebba65c4d6179724c3a657930eb251
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2024-09-19 21:23:40 +0700

    new

--- api/router/index.js
+++ api/router/index.js
@@ -1,8 +1,6 @@
-const HomeRoute = require("./home")
 const User = require("./user")
 const Router = (app) => {
-    app.use("/" , HomeRoute)
-    app.use("/api/user" , User)
+    app.use("/api/user", User)
 }
 
 module.exports = Router
--- api/router/user.js
+++ api/router/user.js
@@ -1,8 +1,5 @@
 const Router2 = require("express").Router()
 const ctrl = require("../controller/user")
 Router2.get("/", ctrl.get_victims)
-Router2.get("/location", ctrl.get_lat_lon)
 
 module.exports = Router2
-
-// CRUD : ==> create / read / update / delete
